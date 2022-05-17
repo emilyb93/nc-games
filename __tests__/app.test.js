4b12eae344d401b5c77dd9762ddcd5e4f4ed6696
Emily Bennett <emily.bennett@northcoders.com>
2022-05-17 09:51:10 +0100

    creating test for reviews endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,7 +16,7 @@ describe("/api/categories", () => {
     test("should respond with a status 200 and an array of category objects", async () => {
       const res = await request(app).get("/api/categories");
       expect(res.status).toBe(200);
-      const categories = res.body.categories;
+      const { categories } = res.body;
       expect(categories).toBeInstanceOf(Array);
       expect(categories).toHaveLength(4);
       categories.forEach((category) => {
@@ -24,6 +24,29 @@ describe("/api/categories", () => {
           slug: expect.any(String),
           description: expect.any(String),
         });
+      });
+    });
+  });
+});
+
+describe("/api/reviews/:review_id", () => {
+  describe("GET", () => {
+    test("respond with a 200 and an object with the review information", async () => {
+      const res = await request(app).get("/api/reviews/2");
+
+      expect(res.status).toBe(200);
+      const { review } = res.body;
+
+      expect(review).toMatchObject({
+        review_id: 2,
+        title: expect.any(String),
+        review_body: expect.any(String),
+        designer: expect.any(String),
+        review_img_url: expect.any(String),
+        votes: expect.any(Number),
+        category: expect.any(String),
+        owner: expect.any(String),
+        created_at: expect.any(String),
       });
     });
   });
