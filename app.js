4f7d288754a9f75bd3fac6e00074fba0be738e79
Emily Bennett <emily.bennett@northcoders.com>
2022-05-20 15:48:29 +0100

    adding psql error handler

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const { sendCategories } = require("./controllers/categories.controller");
 const {
   handle404,
   customErrorHandler,
+  psqlErrorHandler,
 } = require("./controllers/errors.controller.js");
-const { sendReview } = require("./controllers/reviews.controller.js");
-
-const app = require("express")();
+const {
+  sendReview,
+  patchReview,
+} = require("./controllers/reviews.controller.js");
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 
 app.get("/api/categories", sendCategories);
 
-app.route("/api/reviews/:review_id").get(sendReview).patch(updateReview);
+app.route("/api/reviews/:review_id").get(sendReview).patch(patchReview);
 
 app.all("/*", handle404);
+
+app.use(psqlErrorHandler);
 
 app.use(customErrorHandler);
 
--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -5,5 +5,14 @@ exports.handle404 = (req, res, next) => {
 exports.customErrorHandler = (err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+exports.psqlErrorHandler = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    next(err);
   }
 };
