c6e6c2f9c96a134d4ebd8c09106f66feae979b3f
Emily Bennett <emily.bennett@northcoders.com>
2022-05-23 10:23:11 +0100

    adding try catch blocks to controllers

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,7 +1,11 @@
 const { fetchCategories } = require("../models/categories.model.js");
 
 exports.sendCategories = async (req, res, next) => {
-  const categories = await fetchCategories();
+  try {
+    const categories = await fetchCategories();
 
-  res.status(200).send({ categories });
+    res.status(200).send({ categories });
+  } catch (err) {
+    next(err);
+  }
 };
